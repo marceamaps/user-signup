c3eed2be075753b12b5892cb543feb6f59eb5831
marceamaps <marcea.irene@gmail.com>
2018-06-17 15:59:47 -0600

    main.py is working except the email input evaluation

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
 
-@app.route("/")                 # this is my index homepage where errors will show up
+@app.route("/")                 
 def form():
     return render_template('index_homepage.html')
 
@@ -20,44 +20,54 @@ def form_evaluation():
     verify_password = request.form['verify_password']
     # email = request.form['email']
 
+    error_user = ''
+    error_pass = ''
+    error_ver = ''
+
     if not username:
         error_user = "That's not a valid username"
-    
+    elif ' ' in username:
+            error_user = "Please no white spaces"
+    elif len(username) < 3: 
+            error_user = "Please make username between 3 - 20 characters"
+    else:
+        if len(username) > 20:
+            error_user = "Please make username between 3 - 20 characters"
+
     if not password:
         error_pass = "That's not a valid password"
+    elif ' ' in password:
+        error_pass = "Please no spaces within passwords"
+    elif len(password) < 3:
+        error_pass = "Please make password between 3 - 20 characters"
+    else:
+        if len(password) > 20:
+            error_pass = "Please make password between 3 - 20 characters"
 
     if not verify_password:
         error_ver = "That's not a valid password"
-    return render_template('index_homepage.html', error_username=error_user, error_password=error_pass, error_verify_password=error_ver )
-
-    
-
-    
-    
-
-@app.route("/welcome", methods=['POST'])
-def form_welcome():
-    edit_header = "<h2>Edit My Watchlist</h2>"
-
-    new_movie = request.form['new-movie']
-
-    blank_error = ''
-
-    if is_blank(new_movie):
-        error_message = 'Sorry, you did not type anything in. Please try again!'
-        content = page_header + edit_header + add_form.format(blank_error=error_message) + crossoff_form + page_footer
-        return content
+    elif ' ' in verify_password:
+        error_ver = "Please no spaces within passwords"
+    elif len(verify_password) < 3:
+        error_ver = "Please make password between 3 - 20 characters"
+    elif len(verify_password) > 20:
+        error_ver = "Please make password between 3 - 20 characters"
     else:
-        if new_movie in terrible_movies:
-            return redirect('/?error=' + urllib.parse.quote( 
-            "Trust me, you don't want to add '{}' to your list".format(new_movie)))
-        else:
-            new_movie_element = "<strong>" + cgi.escape(new_movie) + "</strong>"
-            sentence = new_movie_element + " has been added to your Watchlist!"
-            content = page_header + "<p>" + sentence + "</p>" + page_footer
-            return content
+        if verify_password != password:
+            error_ver = "Passwords do not match"
+
+    if not error_user and not error_pass and not error_ver:
+        return redirect ('/welcome?username={0}'.format(username))
+    else:
+        return render_template('index_homepage.html', error_username=error_user, error_password=error_pass, error_verify_password=error_ver )
 
 
+@app.route("/welcome")
+def form_welcome():
+
+    user_name = request.args.get('username')
+
+    return render_template('welcome.html', userinput=user_name)
 
 app.run()
 
